5cd77093c4105cf6e847747d75a43502a3b4d011
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2024-02-02 07:02:07 -0300

    testing the text to speech api

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const path = require('path')
+const fs = require('fs')
+
+const OpenAI = require('openai')
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+    model: 'gpt-4',
+  })
+
+  const answer = completion.choices[0].message 
+
+  const speechFile = path.resolve("./speech.mp3")
+
+  const mp3 = await openai.audio.speech.create({
+    model: "tts-1",
+    voice: "alloy",
+    input: answer.content,
+  })
+  console.log(speechFile)
+  const buffer = Buffer.from(await mp3.arrayBuffer())
+  await fs.promises.writeFile(speechFile, buffer)
+}
+
+main()
